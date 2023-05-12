8197413ab8911519332b85110c8f42f076ed1685
Ballphantom <nokaraza012@gmail.com>
2023-05-12 13:33:11 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Popover from "./components/Popover"
+function App() {
+
+  return (
+    <>
+      <Popover/>
+    </>
+  )
+}
+
+export default App
---
+++ src/constant/profile.ts
@@ -0,0 +1,14 @@
+export const profile = [
+  {
+    name: "Ball",
+    description: "Front-end internship",
+  },
+  {
+    name: "Joji",
+    description: "Full-stack internship",
+  },
+  {
+    name: "Diw",
+    description: "Front-end internship",
+  },
+];
